9c72c83bf960e48da20372423d8e761d1c889726
ncoldwave <robbi.mobile@gmail.com>
2025-04-20 00:26:26 +0200

    configuring docusaurus for GitHub Pages

--- ViviComm-Lite-Atlas/docusaurus.config.ts
+++ ViviComm-Lite-Atlas/docusaurus.config.ts
@@ -10,15 +10,16 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/vivicomm-lite-atlas',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'ViviQD', // Usually your GitHub org/user name.
   projectName: 'ViviComm Lite Atlas', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
