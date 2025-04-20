6a29db41dd22040bc7f2340107acea88219cf22c
ncoldwave <robbi.mobile@gmail.com>
2025-04-20 11:52:18 +0200

    change prj link

--- ViviComm-Lite-Atlas/docusaurus.config.ts
+++ ViviComm-Lite-Atlas/docusaurus.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   url: 'https://viviqd.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/vivicomm-lite-atlas/',
+  baseUrl: '/ViviCommLiteAtlas/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
